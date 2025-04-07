1bfd17c632317a9cd1662544e455f4b51efb22ce
Irene Hurtado <irenehurtadoguerrero@gmail.com>
2025-04-07 22:11:15 +0200

    tested-transit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://IreHurtado.github.io",
-    base: "/astro-test",
+    base: "/tested-transit",
     i18n: {
         defaultLocale: "es",
         locales: ["es", "en"],
