e4b20bde72bb96db343ee461228886e8bbcb2715
Irene Hurtado <irenehurtadoguerrero@gmail.com>
2025-04-07 22:10:14 +0200

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://IreHurtado.github.io",
+    base: "/astro-test",
     i18n: {
         defaultLocale: "es",
         locales: ["es", "en"],
